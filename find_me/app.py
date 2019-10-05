d37881c9a178c96ac4d0bb91578e98c6746c1005
Tiago Lira <tiagoliradsantos@gmail.com>
2019-10-05 15:15:21 -0700

    Get client ip

--- find_me/app.py
+++ find_me/app.py
@@ -31,7 +31,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    ip_address = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    ip_address = request.environ['REMOTE_ADDR']
     app.logger.info(f'Finding location for ip {ip_address}')
     location = LocationTracker().track(ip_address)
     app.logger.info(f'Found: {location}')
