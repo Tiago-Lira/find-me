8cece34d642d6f24dce31c022307e267ece60513
Tiago Lira <tiagoliradsantos@gmail.com>
2019-10-05 16:19:10 -0700

    Add more test cases

--- find_me/tests/test_cached_ip_stack_api.py
+++ find_me/tests/test_cached_ip_stack_api.py
@@ -13,8 +13,10 @@ def test_when_ip_is_new_calls_external_service():
 
     with mock as external_service:
         CachedIPStackAPI().get_location('127.0.0.1')
+        CachedIPStackAPI().get_location('127.0.0.2')
+        CachedIPStackAPI().get_location('127.0.0.3')
 
-    external_service.assert_called_once()
+    assert external_service.call_count == 3
 
 
 def test_when_ip_has_been_seen_before_does_not_call_external_service():
@@ -23,7 +25,23 @@ def test_when_ip_has_been_seen_before_does_not_call_external_service():
 
     with mock as external_service:
         api.get_location('127.0.0.1')
-        api.get_location('127.0.0.1')
+        api.get_location('127.0.0.2')
         api.get_location('127.0.0.1')
 
-    external_service.assert_called_once()
+    assert external_service.call_count == 2
+
+
+def test_when_cache_reaches_limit_should_clean_itself():
+    mock = patch('find_me.ipstack.requests.get', return_value=Response("{}"))
+    api = CachedIPStackAPI(max_cache_size=2)
+
+    with mock as external_service:
+        api.get_location('127.0.0.1')
+        api.get_location('127.0.0.1')
+        api.get_location('127.0.0.2')
+        api.get_location('127.0.0.2')
+        api.get_location('127.0.0.3')
+        api.get_location('127.0.0.4')
+        api.get_location('127.0.0.1')
+
+    assert external_service.call_count == 5
