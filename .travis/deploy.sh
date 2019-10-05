4c3a328763c74663e44c6e6334f26d5bf6bdcc31
Tiago Lira <tiagoliradsantos@gmail.com>
2019-10-05 14:21:36 -0700

    Add missing --app

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,4 +2,4 @@
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 echo "$HEROKU_API_KEY" | docker login -u _ --password-stdin registry.heroku.com
 heroku container:push web --app $HEROKU_APP_NAME
-heroku container:release web
+heroku container:release web --app $HEROKU_APP_NAME
